62b4664371c93c62c951dba303a1663eeebcb5c3
scott-alvis <scott-alvis@users.noreply.github.com>
2021-05-25 14:47:57 -0700

    Update header background and logo.

--- search-website/src/components/AppHeader/AppHeader.js
+++ search-website/src/components/AppHeader/AppHeader.js
@@ -8,7 +8,7 @@ export default function AppHeader() {
     <header className="header">
       <nav className="navbar navbar-expand-lg">
         <a className="navbar-brand" href="/">
-          <img src="/images/microsoft-small.svg" className="navbar-logo" alt="Microsoft" />
+          <img src="/images/simon-logo.png" className="navbar-logo" alt="Simon" />
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
