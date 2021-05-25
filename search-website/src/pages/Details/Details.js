5c42c76d142feef255bf0fb1ac22bfea22f03800
scott-alvis <scott-alvis@users.noreply.github.com>
2021-05-25 10:04:51 -0700

    More display fixes.

--- search-website/src/pages/Details/Details.js
+++ search-website/src/pages/Details/Details.js
@@ -42,11 +42,7 @@ export default function Details() {
       detailsBody = (
         <div className="card-body">
           <h5 className="card-title">{document.title}</h5>
-          <img className="image" src={document.image_url} alt="Book cover"></img>
-          <p className="card-text">{document.authors?.join('; ')} - {document.original_publication_year}</p>
-          <p className="card-text">ISBN {document.isbn}</p>
-          <Rating name="half-rating-read" value={parseInt(document.average_rating)} precision={0.1} readOnly></Rating>
-          <p className="card-text">{document.ratings_count} Ratings</p>
+          <img className="image" src={document.image_link} alt="Book cover"></img>
         </div>
       );
     }
