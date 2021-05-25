7bb514929bca4ff3fa7448f3903d29be5b6c92eb
scott-alvis <scott-alvis@users.noreply.github.com>
2021-05-25 11:10:42 -0700

    Updated api config.

--- search-website/api/config.js
+++ search-website/api/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-    SearchIndexName: process.env["SearchIndexName"] || "good-books",
-    SearchApiKey: process.env["SearchApiKey"] || "",
-    SearchServiceName: process.env["SearchServiceName"] || "",
-    SearchFacets: process.env["SearchFacets"] || "authors*,language_code", 
+    SearchIndexName: process.env["SearchIndexName"] || "cosmos-search-index",
+    SearchApiKey: process.env["SearchApiKey"] || "2291A1ABCD2F478768350D6FFB6F2D8A",
+    SearchServiceName: process.env["SearchServiceName"] || "srch-spg",
+    SearchFacets: process.env["SearchFacets"] || "category_names*,brand", 
 }
 if (!CONFIG.SearchIndexName || !CONFIG.SearchApiKey || !CONFIG.SearchServiceName) throw Error("./config.js::Cognitive Services key is missing");
 
